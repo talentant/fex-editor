3131a80e0f51760aae52ffb5f33547804525e231
talentant <talentant@outlook.com>
2018-12-06 22:03:06 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,6 @@
 "use strict";
 
-const fs2 = require("fs");
 const distDir = "dist/";
-const cssBasePath = "themes/default/_css/";
-const jsBasePath = "_src/";
-const parseBasePath = "_parse/";
-
-function fetchScripts(readFile, basePath) {
-  let sources = fs2.readFileSync(readFile);
-
-  sources = /\[([^\]]+\.js'[^\]]+)]/.exec(sources);
-  sources = sources[1].replace(/\/\/.*\n/g, "\n").replace(/'|"|\n|\t|\s/g, "");
-  sources = sources.split(",");
-  sources.forEach((filepath, index) => {
-    sources[index] = basePath + filepath;
-  });
-
-  return sources;
-}
-
-function fetchStyles () {
-  const sources = fs2.readFileSync(cssBasePath + "ueditor.css");
-  let filepath;
-  const pattern = /@import\s+([^;]+)*;/g;
-  const src = [];
-
-  while ((filepath = pattern.exec(sources))) {
-    src.push(cssBasePath + filepath[1].replace(/['"]/g, ""));
-  }
-
-  return src;
-}
 
 function updateConfigFile (grunt) {
   const filename = "ueditor.config.js";
@@ -63,7 +33,125 @@ module.exports = (grunt) => {
             return "// " + filename + "\n" + src.replace("/_css/", "/css/") + "\n";
           }
         },
-        src: fetchScripts("_examples/editor_api.js", jsBasePath),
+        src: [
+          '_src/editor.js',
+          '_src/core/browser.js',
+          '_src/core/utils.js',
+          '_src/core/EventBase.js',
+          '_src/core/dtd.js',
+          '_src/core/domUtils.js',
+          '_src/core/Range.js',
+          '_src/core/Selection.js',
+          '_src/core/Editor.js',
+          '_src/core/Editor.defaultoptions.js',
+          '_src/core/loadconfig.js',
+          '_src/core/ajax.js',
+          '_src/core/filterword.js',
+          '_src/core/node.js',
+          '_src/core/htmlparser.js',
+          '_src/core/filternode.js',
+          '_src/core/plugin.js',
+          '_src/core/keymap.js',
+          '_src/core/localstorage.js',
+          '_src/plugins/defaultfilter.js',
+          '_src/plugins/inserthtml.js',
+          '_src/plugins/autotypeset.js',
+          '_src/plugins/autosubmit.js',
+          '_src/plugins/background.js',
+          '_src/plugins/image.js',
+          '_src/plugins/justify.js',
+          '_src/plugins/font.js',
+          '_src/plugins/link.js',
+          '_src/plugins/iframe.js',
+          '_src/plugins/scrawl.js',
+          '_src/plugins/removeformat.js',
+          '_src/plugins/blockquote.js',
+          '_src/plugins/convertcase.js',
+          '_src/plugins/indent.js',
+          '_src/plugins/print.js',
+          '_src/plugins/preview.js',
+          '_src/plugins/selectall.js',
+          '_src/plugins/paragraph.js',
+          '_src/plugins/directionality.js',
+          '_src/plugins/horizontal.js',
+          '_src/plugins/time.js',
+          '_src/plugins/rowspacing.js',
+          '_src/plugins/lineheight.js',
+          '_src/plugins/insertcode.js',
+          '_src/plugins/cleardoc.js',
+          '_src/plugins/anchor.js',
+          '_src/plugins/wordcount.js',
+          '_src/plugins/pagebreak.js',
+          '_src/plugins/wordimage.js',
+          '_src/plugins/dragdrop.js',
+          '_src/plugins/undo.js',
+          '_src/plugins/copy.js',
+          '_src/plugins/paste.js',
+          '_src/plugins/puretxtpaste.js',
+          '_src/plugins/list.js',
+          '_src/plugins/source.js',
+          '_src/plugins/enterkey.js',
+          '_src/plugins/keystrokes.js',
+          '_src/plugins/fiximgclick.js',
+          '_src/plugins/autolink.js',
+          '_src/plugins/autoheight.js',
+          '_src/plugins/autofloat.js',
+          '_src/plugins/video.js',
+          '_src/plugins/table.core.js',
+          '_src/plugins/table.cmds.js',
+          '_src/plugins/table.action.js',
+          '_src/plugins/table.sort.js',
+          '_src/plugins/contextmenu.js',
+          '_src/plugins/shortcutmenu.js',
+          '_src/plugins/basestyle.js',
+          '_src/plugins/elementpath.js',
+          '_src/plugins/formatmatch.js',
+          '_src/plugins/searchreplace.js',
+          '_src/plugins/customstyle.js',
+          '_src/plugins/catchremoteimage.js',
+          '_src/plugins/snapscreen.js',
+          '_src/plugins/insertparagraph.js',
+          '_src/plugins/webapp.js',
+          '_src/plugins/template.js',
+          '_src/plugins/music.js',
+          '_src/plugins/autoupload.js',
+          '_src/plugins/autosave.js',
+          '_src/plugins/charts.js',
+          '_src/plugins/section.js',
+          '_src/plugins/simpleupload.js',
+          '_src/plugins/serverparam.js',
+          '_src/plugins/insertfile.js',
+          '_src/ui/ui.js',
+          '_src/ui/uiutils.js',
+          '_src/ui/uibase.js',
+          '_src/ui/separator.js',
+          '_src/ui/mask.js',
+          '_src/ui/popup.js',
+          '_src/ui/colorpicker.js',
+          '_src/ui/tablepicker.js',
+          '_src/ui/stateful.js',
+          '_src/ui/button.js',
+          '_src/ui/splitbutton.js',
+          '_src/ui/colorbutton.js',
+          '_src/ui/tablebutton.js',
+          '_src/ui/autotypesetpicker.js',
+          '_src/ui/autotypesetbutton.js',
+          '_src/ui/cellalignpicker.js',
+          '_src/ui/pastepicker.js',
+          '_src/ui/toolbar.js',
+          '_src/ui/menu.js',
+          '_src/ui/combox.js',
+          '_src/ui/dialog.js',
+          '_src/ui/menubutton.js',
+          '_src/ui/multiMenu.js',
+          '_src/ui/shortcutmenu.js',
+          '_src/ui/breakline.js',
+          '_src/ui/message.js',
+          '_src/adapter/editorui.js',
+          '_src/adapter/editor.js',
+          '_src/adapter/message.js',
+          '_src/adapter/autosave.js',
+        ],
         dest: distDir + packageJson.name + ".all.js"
       },
       parse: {
@@ -71,11 +159,43 @@ module.exports = (grunt) => {
           banner: banner + "(function(){\n\n",
           footer: "\n\n})();\n"
         },
-        src: fetchScripts("ueditor.parse.js", parseBasePath),
+        src: [
+          '_parse/parse.js',
+          '_parse/insertcode.js',
+          '_parse/table.js',
+          '_parse/charts.js',
+          '_parse/background.js',
+          '_parse/list.js',
+          '_parse/video.js',
+        ],
         dest: distDir + packageJson.name + ".parse.js"
       },
       css: {
-        src: fetchStyles(),
+        src: [
+          'themes/default/_css/uibase.css',
+          'themes/default/_css/toolbar.css',
+          'themes/default/_css/editor.css',
+          'themes/default/_css/menubutton.css',
+          'themes/default/_css/menu.css',
+          'themes/default/_css/combox.css',
+          'themes/default/_css/button.css',
+          'themes/default/_css/buttonicon.css',
+          'themes/default/_css/splitbutton.css',
+          'themes/default/_css/popup.css',
+          'themes/default/_css/message.css',
+          'themes/default/_css/dialog.css',
+          'themes/default/_css/paragraphpicker.css',
+          'themes/default/_css/tablepicker.css',
+          'themes/default/_css/colorpicker.css',
+          'themes/default/_css/autotypesetpicker.css',
+          'themes/default/_css/cellalignpicker.css',
+          'themes/default/_css/separtor.css',
+          'themes/default/_css/colorbutton.css',
+          'themes/default/_css/multiMenu.css',
+          'themes/default/_css/contextmenu.css',
+          'themes/default/_css/shortcutmenu.css',
+          'themes/default/_css/pastepicker.css',
+        ],
         dest: distDir + "themes/default/css/ueditor.css"
       }
     },
@@ -125,18 +245,6 @@ module.exports = (grunt) => {
         ]
       }
     },
-    clean: {
-      build: {
-        src: [
-          distDir + "jsp/src",
-          distDir + "*/upload",
-          distDir + ".DS_Store",
-          distDir + "**/.DS_Store",
-          distDir + ".git",
-          distDir + "**/.git"
-        ]
-      }
-    }
   });
 
   grunt.loadNpmTasks("grunt-text-replace");
@@ -144,7 +252,6 @@ module.exports = (grunt) => {
   grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-clean");
 
   grunt.registerTask("default", "build all", function() {
     updateConfigFile(grunt);
@@ -153,7 +260,6 @@ module.exports = (grunt) => {
       "cssmin",
       "uglify",
       "copy:base",
-      "clean"
     ]);
   });
 };
